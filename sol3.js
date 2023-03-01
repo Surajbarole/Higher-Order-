f753ef0a7f04a54bd3a2fc379679e90051af1106
Suraj Barole <103420352+Surajbarole@users.noreply.github.com>
2023-03-01 05:34:02 +0000

    Initial commit

---
+++ sol3.js
@@ -0,0 +1,15 @@
+// Problem 3: Taking the following array into consideration get the output as given below:
+// arr = [3,4,5,6,7]
+// Output ==> 3 - 5 - 7
+// Hint: You have to use forEach and conditional statement over here.
+
+let arr = [3,4,5,6,7];
+let bag="";
+let array=arr.forEach(function(element,index){
+  if(index%2==1){
+    bag=bag+"-"
+  }else{
+    bag=bag+element
+  }
+})
+console.log(bag)
